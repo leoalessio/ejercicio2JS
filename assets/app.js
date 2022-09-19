a3732976cc79c30af7b1f805d28cad1ffa7d149d
Leo Alessio <leoalessio@outlook.com.ar>
2022-09-18 22:21:40 -0300

    modificamos para ejercicio 3

--- assets/app.js
+++ assets/app.js
@@ -3,36 +3,49 @@ const pizzas = [
       id: 1,
       nombre: "Pizza Especial",
       precio: 700,
+      ingredientes:["salsa de tomates", "queso muzzarella", "Jamón", "Morrón", "aceitunas"],
+      img: "https://www.region20.com.ar/images/4/5/1/6/1/0/451610_0.jpg"
     },
   
     {
       id: 2,
       nombre: "Pizza 4 Quesos",
       precio: 800,
+      ingredientes:["queso muzzarella", "queso roquefort", "queso gorgonzola", "queso parmesano", "aceitunas"],
+      img: "https://assets.unileversolutions.com/recipes-v2/216503.jpg?imwidth=900"
+
     },
   
     {
       id: 3,
       nombre: "Pizza Muzzarella",
       precio: 600,
+      ingredientes:["salsa de tomates", "queso muzzarella", "aceitunas"],
+      img: "https://unareceta.com/wp-content/uploads/2014/05/pizza-4-quesos.jpg"
     },
   
     {
       id: 4,
       nombre: "Pizza Rúcula y Crudo",
       precio: 900,
+      ingredientes:["salsa de tomates", "queso muzzarella", "Jamón Crudo", "Rúcula"],
+      img:"https://www.lavanguardia.com/files/image_948_465/uploads/2020/01/08/5e997a4340ab2.jpeg"
     },
   
     {
       id: 5,
       nombre: "Pizza Con Palmitos",
       precio: 900,
+      ingredientes:["salsa de tomates", "queso muzzarella", "palmitos", "salsa golf"],
+      img:"https://www.supermercedes.com.ar/napoles/wp-content/uploads/2020/05/pizzapalmitos-768x454.jpg"
     },
   
     {
       id: 6,
       nombre: "Pizza Napolitana",
       precio: 800,
+      ingredientes:["salsa de tomates", "queso muzzarella", "Tomate en rodajas", "ajo"],
+      img:"https://www.johaprato.com/files/styles/immagini_ricette/public/pizza-napolitana.jpg?itok=eFnvW--t"
     },
   ];
 
@@ -41,6 +54,13 @@ const addForm = document.getElementById('form');
 const btnSend = document.querySelector('.btn-send');
 const inputId = document.querySelector('.input-id');
 const resultRender = document.querySelector('.container-result');
+
+
+
+const saveLocalStorage = listaDePizzas => {
+  localStorage.setItem('pizzas', JSON.stringify(listaDePizzas));
+};
+
 
 
 const addResult = e => {
@@ -52,7 +72,7 @@ const addResult = e => {
     if (valueInput == 0) {
         resultRender.innerHTML =`  <div class="result">
         
-        <h4>"Pusiste 0 y te dije que tiene que ser del 1 a 6" </h4>
+        <h4>"Por favor, ingrese un valor válido" </h4>
         </div>`
         return ;
     }
@@ -60,7 +80,7 @@ const addResult = e => {
     if(Math.sign(valueInput)=== -1){
         resultRender.innerHTML =`  <div class="result">
     
-        <h4>"Pusiste un número negativo y te dije que tiene que ser del 1 a 6" </h4>
+        <h4>"Por favor, ingrese un valor válido" </h4>
         </div>`
         return; 
     }
@@ -70,7 +90,10 @@ const addResult = e => {
         search.forEach((pizza) =>
         
         resultRender.innerHTML = `  <div class="result">
-                                    ${pizza.nombre} 
+                                    <p class="h2-cards">${pizza.nombre}</p> 
+                                    <p class="p-cards">${pizza.ingredientes} </p>
+                                    <img src="${pizza.img}" alt="se rompió" class="img-pizza"/> 
+
                                     <h4>$ ${pizza.precio}</h4>
                                     </div>`);
                                     
@@ -78,7 +101,7 @@ const addResult = e => {
     }else{
         resultRender.innerHTML = `  <div class="result">
     
-        <h4>"Seguí las instrucciones una vez, te dije del 1 a 6" </h4>
+        <h4>"Por favor, ingrese un valor válido" </h4>
         </div>`;
         return;
     }
@@ -89,6 +112,7 @@ const addResult = e => {
 
 const init = () => {
     addForm.addEventListener('submit', addResult);
+    saveLocalStorage(pizzas)
 };
 
 init();
